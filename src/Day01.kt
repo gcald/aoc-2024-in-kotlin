9eee244f91c7d89b758a397ba43dc0a42e5240ef
George Calderon <gcald117@gmail.com>
2024-11-30 21:48:17 -0800

    Day 1 part 2 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -23,7 +23,48 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val list1 = mutableListOf<Int>()
+        val list2 = mutableListOf<Int>()
+        input.forEach { line ->
+            val entries = line.split("   ")
+            list1.add(entries[0].toInt())
+            list2.add(entries[1].toInt())
+        }
+
+        list1.sort()
+        list2.sort()
+
+        val numMap = mutableMapOf<Int, Int>()
+
+        list1.forEach { number ->
+            if (numMap.containsKey(number)) {
+                return@forEach
+            }
+
+            numMap.put(number, 0)
+        }
+
+        list2.forEach { number ->
+            if (!numMap.containsKey(number)) {
+                return@forEach
+            }
+
+            var sum = numMap[number]
+
+            if (sum === null) {
+                return@forEach
+            }
+
+            numMap.put(number, sum + 1)
+        }
+
+        var sum = 0
+
+        numMap.forEach { number, multiplier ->
+            sum += number * multiplier
+        }
+
+        return sum
     }
 
     // Read the input from the `src/Day01.txt` file.
