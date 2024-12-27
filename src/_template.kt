11771ff546a864804a27f5e327df7a29ef0fbd70
George Calderon <gcald117@gmail.com>
2024-12-26 20:07:33 -0800

    Added template solution file

---
+++ src/_template.kt
@@ -0,0 +1,17 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Any {
+
+        return -1
+    }
+
+    fun part2(input: List<String>): Int {
+        return -1
+    }
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("Day01")
+    part1(input).println()
+    part2(input).println()
+}
